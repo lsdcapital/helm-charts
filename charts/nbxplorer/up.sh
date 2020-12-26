b0062801751de11d73640c8d67d748a446a260fa
Stefan Lesicnik <stefan@lsdopen.io>
2020-12-26 14:50:07 +0200

    nbxplorer

--- charts/nbxplorer/up.sh
+++ charts/nbxplorer/up.sh
@@ -1,2 +1,5 @@
     helm upgrade -i nbxplorer . -n playpen --create-namespace \
+    --set rabbitmq.auth.password='test123' \
+    --set rabbitmq.auth.erlangCookie='test123' \
+    --set config.rpcauth='lsdopenlnd:PItVvAqMJl_OmpQc54jVIUpCMM9rLEmhoF2h4GHEH8E='
     $@
